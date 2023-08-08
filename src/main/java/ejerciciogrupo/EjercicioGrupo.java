1ccac2f9b340134e883f543d618feacff90a8eaf
gregorionunez <colorc98@gmail.com>
2023-08-08 19:29:51 -0300

    Initial commit

---
+++ src/main/java/ejerciciogrupo/EjercicioGrupo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejerciciogrupo;
+
+/**
+ *
+ * @author Gregorio
+ */
+public class EjercicioGrupo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
